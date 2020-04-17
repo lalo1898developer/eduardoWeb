228b13b9ca1f4dbf292c1c5ca78dd608eecff477
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-04-17 16:05:40 -0500

    Update server.js

--- WebDevelopmentBootcamp/my-express-server/server.js
+++ WebDevelopmentBootcamp/my-express-server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(request, response){
-    response.send("Hello");
+    response.send("<h1>Hello World!<h1/>");
 });
 
 app.listen(3000, function(){

1bfafc26da864c93324cf1ddf5a1a7b3010cb56c
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-04-17 23:10:25 -0500

    Create project Calculator

--- WebDevelopmentBootcamp/my-express-server/server.js
+++ WebDevelopmentBootcamp/my-express-server/server.js
@@ -5,6 +5,18 @@ app.get("/", function(request, response){
     response.send("<h1>Hello World!<h1/>");
 });
 
+app.get("/contact", function(request, response){
+    response.send("Contactame a: lalo.work.tech@gmail.com");
+});
+
+app.get("/about", function(request, response){
+    response.send("Soy Eduardo Rodriguez Ricardez");
+});
+
+app.get("/hobbies", function(request, response){
+    response.send("Juego tocho y me gusta tomar cursos");
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });

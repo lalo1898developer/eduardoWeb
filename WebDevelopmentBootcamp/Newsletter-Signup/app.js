dabab29299e4ef6dd07be198db0664b9949b2da1
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-04-19 20:41:51 -0500

    Update project Neslatter

--- WebDevelopmentBootcamp/Newsletter-Signup/app.js
+++ WebDevelopmentBootcamp/Newsletter-Signup/app.js
@@ -38,6 +38,12 @@ app.post("/", function(req, res){
         auth: "lalo1898jr:07588204ba50042d87d13742e9772f65-us4"
     }
     const request = https.request(url, options, function(response){
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
@@ -45,9 +51,13 @@ app.post("/", function(req, res){
 
     request.write(jsonData);
     request.end();
-})
+});
 
-app.listen(3000, function(){
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server runnig on port 3000");
 });
 

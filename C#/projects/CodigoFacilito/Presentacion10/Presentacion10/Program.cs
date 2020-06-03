d2c6cf97a066e73e047cf98ad3357b9ec1d56c95
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-06-03 10:18:00 -0500

    Create AplicacionPedidos, Presentacion10 & Presentacion9

---
+++ C#/projects/CodigoFacilito/Presentacion10/Presentacion10/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[,] matriz = new int[3,3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("Dame el valor {0}, {1}", i, j);
+                    matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("El valor de la posición {0}, {1} es: {2}", i, j, matriz[i, j]);
+                }
+            }
+
+            Console.ReadKey();*/
+
+            int[][] matriz = new int[3][];
+            matriz[0] = new int[1];
+            matriz[1] = new int[3];
+            matriz[2] = new int[2];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < matriz[i].Length; j++)
+                {
+                    Console.WriteLine("Dame el valor {0}, {1}", i, j);
+                    matriz[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < matriz[i].Length; j++)
+                {
+                    Console.WriteLine("El valor de la posición {0}, {1} es: {2}", i, j, matriz[i][j]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/projects/CodigoFacilito/Presentacion9/Presentacion9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arreglo = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Dame el valor de {0}", i);
+                arreglo[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("El valor del elemento {0} es: {1}", i, arreglo[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

4e10c4d22408238e9c2874c0df8b283823bc3356
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-06-05 06:33:05 -0500

    Update: "/Pages/Shared/_Layout.cshtml" "Program.cs"
    Create:
    "/Models/SeenData.cs"
    "CodigoFacilito/Presentacion11"

---
+++ C#/projects/CodigoFacilito/Presentacion11/Presentacion11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion11
+{
+    class Program
+    {
+        static int[] arreglo = { 1, 2, 3, 4, 5, 6, -1 };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame un valor a buscar en el arreglo:");
+            int valorBuscar = Convert.ToInt32(Console.ReadLine());
+
+            ref int valor = ref BuscarValores(valorBuscar);
+
+            if (valor != -1)
+            {
+                Console.WriteLine("EL valor encontraod es {0}", valor);
+            }
+            else
+            {
+                Console.WriteLine("No se encontro valor");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static ref int BuscarValores(int valor)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                if (arreglo[i] == valor)
+                {
+                    return ref arreglo[i];
+                }
+            }
+
+            return ref arreglo[6];
+        }
+    }
+}
